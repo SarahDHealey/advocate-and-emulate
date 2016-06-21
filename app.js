289ada43801b6750ac23aeb4f085b5ad7826620c
Sarah Healey <s.deane.healey@gmail.com>
2016-06-21 17:01:48 -0600

    scraping works in node (need server or browserify, over my head) does not work in browser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 jQuery(document).ready(function ($) {
 
+//ajax calls:
+
     $.ajax({
         url: "https://api.instagram.com/v1/tags/cat/media/recent?access_token=245508994.f6b7f6d.a9a85308fa354d4aa02a4456b1da504e",
         dataType: "jsonp", // this is important to circumvent cross-domain issues
@@ -22,20 +24,50 @@ jQuery(document).ready(function ($) {
     });
 
     $.ajax({
-        url: "https://api.shopstyle.com/api/v2/products?pid=uid8689-34352617-46&fts=leopardprint&offset=0&limit=10",
+        url: "https://api.shopstyle.com/api/v2/products?pid=uid8689-34352617-46&fts=Toms+turtles&offset=0&limit=10",
         // dataType: "jsonp", // this is important to circumvent cross-domain issues
         // cache: false,
         success: function (response) {
           var responseArray = response.products;
           var imagesArray = [];
+          var productArray = [];
           responseArray.forEach(function(element) {
             imagesArray.push(element.image.sizes);
+            productArray.push(element);
           });
-          imagesArray.forEach(function(element) {
-            console.log(element.XLarge.url);
-            var newNode = $("<img>").attr('src', element.XLarge.url);
-            $(".row").append(newNode);
+          productArray.forEach(function(element){
+            $(".row").append($("<a />").attr({class: 'clickUrl', href:element.clickUrl}).html("<img src='" + element.image.sizes.XLarge.url + "'>"));
+
+
+            // $('<img />').attr({
+            //   src:'some image url',
+            //   width:'width in intiger',
+            //   height:'integer'
+            // }).appendTo($('<a />').attr({
+            //   href:'somelink'
+            // }).appendTo($('#someElement')));
+
+            // var newImg = ($"")
+            // $("<a>").append($"<img>")element.image.sizes.XLarge.url);
+            // var newNode = ($("<img>")).attr({src: element.image.sizes.XLarge.url});
+            // ('.clickUrl').append(newNode);
+            // var newImg = $("<img>").attr('src', element.XLarge.url);
+              // $("<a>").append(newImg);
+            // // console.log(newLink);
+            // $(".row").append(newLink);
+            // var newImg = $("<img>").attr('src', element.image.sizes.XLarge.url);
+            // console.log(newImg);
+            // $(".clickUrl").html(newImg);
           });
+
+          // imagesArray.forEach(function(element) {
+          //   console.log(element.XLarge.url);
+          //   var newImg = $("<img>").attr('src', element.XLarge.url);
+          //     $("<a>").append(newImg);
+          // });
+          // imagesArray.forEach(function(element) {
+
+          // });
 
           // imagesArray.forEach(function(element) {
             // console.log(element);
